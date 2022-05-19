8deb7f1eeab5a9e1dba6157d0416cfa7e8952301
ewaldmikolaj <mikolajewald@gmail.com>
2022-05-19 22:49:33 +0200

    avg values calculated

--- script.r
+++ script.r
@@ -1,2 +1,12 @@
-medical_data <- read.csv2("./data/przykladoweDane-Projekt.csv", sep = ";")
+data <- read.csv2("./data/przykladoweDane-Projekt.csv", sep = ";")
 
+num_cols <- unlist(lapply(data, is.numeric))
+
+values <- data[, num_cols]
+groups <- data[, !num_cols]
+avg_values <- aggregate(values, groups, mean, na.rm = T)
+
+na_values <- data.frame(which(is.na(data), arr.ind = TRUE))
+
+
+#data[na_values$row, !num_cols]
