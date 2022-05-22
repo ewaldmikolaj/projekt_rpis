b3d4ce959b9b7e8a0d5249e9fb9eca5500a53d9c
ewaldmikolaj <mikolajewald@gmail.com>
2022-05-22 18:21:31 +0200

    filled empty values and edited md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# projekt_rpis
+<h1 align="center"> projekt RPiS 2022 </h1>
+Mikołaj Ewald, 469494
+
+&nbsp;<br>
+
+## manual uruchomienia programu
+
+&nbsp;<br>
+
+## sprawozdanie
+
+### przygotowanie danych wejściowych
+
+W celu przygotowania danych wyjściowych w pliku [*przykladoweDane-Projekt.csv*](https://github.com/ewaldmikolaj/projekt_rpis/blob/main/data/przykladoweDane-Projekt.csv) zacząłem od uzupełnienia braków w danych, czyli wartości NA.
--- script.r
+++ script.r
@@ -1,12 +1,25 @@
+library("dplyr")
+library("data.table")
+
 data <- read.csv2("./data/przykladoweDane-Projekt.csv", sep = ";")
 
 num_cols <- unlist(lapply(data, is.numeric))
-
 values <- data[, num_cols]
-groups <- data[, !num_cols]
-avg_values <- aggregate(values, groups, mean, na.rm = T)
+avg_values <- aggregate(values, data[1], mean, na.rm = T)
+avg_values
 
-na_values <- data.frame(which(is.na(data), arr.ind = TRUE))
+#zamiana braków dancu na średnie dla odpowiedniej grupy
+for (i in which(sapply(data, is.numeric))) {
+  for (j in which(is.na(data[, i]))) {
+    data[j, i] <- mean(data[data[, 1] == data[j, 1], i], na.rm = TRUE)
+  }
+}
+
+#wykrycie wartości odstających
+for (i in which(sapply(data, is.numeric))) {
+  cat("Wartosci odstajace w", names(data)[i], ":", boxplot(data[i], 
+    plot = FALSE)$out, "\n")
+}
 
 
-#data[na_values$row, !num_cols]
+data.table(aggregate(values, data[1], mean))
