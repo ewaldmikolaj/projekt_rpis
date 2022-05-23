015c885b17a0e0290c7b9a91ce124bdece0d7bce
ewaldmikolaj <mikolajewald@gmail.com>
2022-05-23 23:03:44 +0200

    added characteristic and edited readme

--- README.md
+++ README.md
@@ -11,4 +11,23 @@ Mikołaj Ewald, 469494
 
 ### przygotowanie danych wejściowych
 
-W celu przygotowania danych wyjściowych w pliku [*przykladoweDane-Projekt.csv*](https://github.com/ewaldmikolaj/projekt_rpis/blob/main/data/przykladoweDane-Projekt.csv) zacząłem od uzupełnienia braków w danych, czyli wartości NA.
+W celu przygotowania danych wyjściowych w pliku [*przykladoweDane-Projekt.csv*](https://github.com/ewaldmikolaj/projekt_rpis/blob/main/data/przykladoweDane-Projekt.csv) zacząłem od uzupełnienia braków w danych, czyli wartości NA. Wartości puste uzupełniane są przez **średnią** obliczaną dla grupy znajdującej się w pierwszej kolumnie zbioru danych. <br>
+Wartości NA uzupełnione przez program:
+
+- 5 rząd, 10 kolumna (gr: CHOR1, kl: MON): 0.8579167
+- 13 rząd, 7 kolumna (gr: CHOR1, kl: HGB): 12.41141
+- 68 rząd, 7 kolumna (gr: KONTROLA, kl: HGB): 11.26357
+
+Następnie w celu oceny danych program raportuje **wartości odstające**, czyli wartości, które są odegłe od pozostałych elementów próby. Zazwyczaj takie wartości świadczą o błędach danych spowodowanych błędnym pomiarem lub pomyłce we wprowadzaniu danych. 
+
+Wartości odstające w analizowanym plik:
+
+| kolumna | wartości                        |
+|---------|---------------------------------|
+| hsCRP   | 20.1548 16.4069 42.6499 19.2124 |
+| ERY     | 33                              |
+| PLT     | 456 434                         |
+| HGB     | 22.2318                         |
+| HCT     | 0.0423                          |
+| MCHC    | 38.8674 38.203 32.0573 32.2234  |
+| MON     | 1.5 1.52 0.14 1.61 7            |
--- script.r
+++ script.r
@@ -1,4 +1,6 @@
 library("dplyr")
+library("gridExtra")
+library("xlsx")
 library("data.table")
 
 data <- read.csv2("./data/przykladoweDane-Projekt.csv", sep = ";")
@@ -9,17 +11,41 @@ avg_values <- aggregate(values, data[1], mean, na.rm = T)
 avg_values
 
 #zamiana braków dancu na średnie dla odpowiedniej grupy
+write("Raport z zamiany braków", file = "./raport.txt")
 for (i in which(sapply(data, is.numeric))) {
   for (j in which(is.na(data[, i]))) {
-    data[j, i] <- mean(data[data[, 1] == data[j, 1], i], na.rm = TRUE)
+    new_value <- mean(data[data[, 1] == data[j, 1], i], na.rm = TRUE)
+    data[j, i] <- new_value
+
+    text <- c("Zamiana w", j, "i", i, "na", new_value)
+    write(text, "./raport.txt", sep = " ", append = TRUE,
+      ncolumns = length(text))
+
+    #cat(text, "\n", sep = " ")
   }
 }
 
 #wykrycie wartości odstających
+write("\nWartosci odstające w danych", file = "./raport.txt", append = TRUE)
 for (i in which(sapply(data, is.numeric))) {
-  cat("Wartosci odstajace w", names(data)[i], ":", boxplot(data[i], 
-    plot = FALSE)$out, "\n")
+  text <- c("Wartosci odstajace w kolumnie", names(data)[i], ":",
+    boxplot(data[i], plot = FALSE)$out)
+  write(text, "./raport.txt", sep = " ", append = TRUE,
+    ncolumns = length(text))
+
+  #cat(text, "\n", sep = " ")
 }
 
+data_mean <- data.frame(
+  data %>%
+    group_by(data[1]) %>%
+    summarise(across(where(is.numeric), mean))
+)
+#cat("Charakterystyka danych: srednie wartosci:\n")
+#data_mean
 
-data.table(aggregate(values, data[1], mean))
+data_mean <- data.frame(
+  data %>%
+    group_by(data[1]) %>%
+    summarise(across(where(is.numeric), mean))
+)
